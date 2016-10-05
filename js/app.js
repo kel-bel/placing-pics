09e832fe1f98fdb9ee91f7e628378c33044fcf2f
Kelly Ceriani <kelly5@vt.edu>
2016-10-05 14:21:19 -0400

    js/

--- js/app.js
+++ js/app.js
@@ -7,22 +7,38 @@ var initMap =  function() {
 	});
 };
 
+var showPictures = function(data) {
+	var html = "";
+	$.each(data, function(index, value) {
+		console.log(index);
+		console.log(value); 
+		html += '<img src="' + value.photos.images.url + '"/>';
+		console.log('The latitude is ' + value.photos.latitude);
+		console.log('The longitude is ' + value.photos.longitude);
+	});
+	$('.pictures').html(html);
+};
+
 var getPhotos = function(location) {
 	console.log(location);
 	//parameter to get Photos from 500px API
 	var params = {
-		tag: location
+		tag: location,
+		part: 'photos'
 	};
 
 	$.ajax({
 		url: "https://api.500px.com/v1/photos/search?consumer_key=jbENq5GFQfuSSut8cQQSbRH6sRjNoxVehMwJZIwp",
 		data: params,
-		type: "GET"
-	});
+		dataType: "json",
+		type: "GET",
+	})
 
-
-	$.getJSON(url, params, function(data) {
+	.done( function(data) {
 		console.log(data);
+		//show the pics from 500px 
+		showPictures(data);
+
 	});
 };
 
