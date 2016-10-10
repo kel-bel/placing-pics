bd455732d08541547ea22ce9f74bea5753fd25f8
Kelly Ceriani <kelly5@vt.edu>
2016-10-10 14:28:39 -0400

    js/

--- js/app.js
+++ js/app.js
@@ -1,15 +1,19 @@
 function initMap() {
+	var startingLatLng = {lat: 40.7128, lng: -73.9352};
+
 	var map = new google.maps.Map(document.getElementById('map'), {
 		zoom: 8,
-		center: {lat: 40.7128, lng: -73.9352}
+		center: startingLatLng
 	});
 
 	//geocoding area, when submit it finds address lat/long
 	var geocoder = new google.maps.Geocoder();
 
 	document.getElementById('submit').addEventListener('click', function() {
-			geocodeAddress(geocoder, map);
-			//getPhotos(map);
+		geocodeAddress(geocoder, map);
+	
+		//Identifying the given Lat and Longitude the map starts with	
+		console.log(startingLatLng);
 	});
 };
 
@@ -17,32 +21,19 @@ function geocodeAddress(geocoder, resultsMap) {
 	var address = document.getElementById('address').value;
 	geocoder.geocode({'address': address}, function(results, status) {
 		if (status === 'OK') {
+			console.log(results[0].geometry.location);
 			resultsMap.setCenter(results[0].geometry.location);
-			var marker = new google.maps.Marker({
-				map: resultsMap,
-				position: results[0].geometry.location
-			});
-			getPhotos(address);
+			getPhotosFromSource(address, resultsMap);
 		} else {
-			alert('Geocode was not successful for the following reasons: ' + status);
+			alert('Please write a city name. Just the city name!')
+			console.log('Geocode was not successful for the following reasons: ' + status);
 		}
 	});
 };
 
-function showPictures(data) {
-	var html = "";
-	$.each(data.photos, function(index, value) {
-		console.log(index);
-		console.log(value); 
-		html += '<img src="' + value.image_url + '"/>';
-		console.log('The latitude is ' + value.latitude);
-		console.log('The longitude is ' + value.longitude);
-	});
-	$('.pictures').html(html);
-};
-
-function getPhotos(address) {
+function getPhotosFromSource(address, resultsMap) {
 	console.log(address);
+	console.log(resultsMap);
 	$('.pictures').html('');
 	//parameter to get Photos from 500px API
 	var params = {
@@ -60,16 +51,32 @@ function getPhotos(address) {
 	.done( function(data) {
 		console.log(data);
 		//show the pics from 500px 
-		showPictures(data);
+		setPictures(data);
 
 	});
 };
 
-//$(document).ready(function() {
-//	$('.location-getter').submit( function(e) {
-//		e.preventDefault();
-//		$('.pictures').html('');
-//		var location = $(this).find("input[name='location']").val();
-//		getPhotos(location);
-//	}); 
-//});
+function setPictures(data) {
+	var html = "";
+	$.each(data.photos, function(index, value) {
+		console.log(index);
+		console.log(value); 
+		html += '<img src="' + value.image_url + '"/>';
+		console.log('Latitude: ' + value.latitude + 'Longitude: ' + value.longitude);
+		
+		//add markers to each location on map
+		var marker = new google.maps.Marker({
+			map: map,
+			position: value.latitude + value.longitude,
+			icon: value.image_url
+		});
+
+		//to add the marker to the map, you must call setMap?
+		marker.setMap(map);
+	});
+	$('.pictures').html(html);
+
+	
+	
+};
+
