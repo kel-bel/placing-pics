6ffab9bbb096090f309b7c94f0f364cae27eaa4f
kel-bel <kelly5@vt.edu>
2016-10-22 18:03:31 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,52 +1,52 @@
-var map;
-var initMap =  function() {
-	map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 2,
-		center: new google.maps.LatLng(2.8,-187.3),
-		mapTypeId: 'terrain'
+function initMap() {
+	var startingLatLng = {lat: 40.7531, lng: -73.9612};
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 13,
+		center: startingLatLng
+	});
+
+	//geocoding area, when submit it finds address lat/long
+	var geocoder = new google.maps.Geocoder();
+	document.getElementById('submit').addEventListener('click', function() {
+		geocodeAddress(geocoder, map);
+	
+
+		//identifying Markers
+		var keyWord = document.getElementById('address').value;
+		var displayOnMap = getPhotosFromSource(keyWord);
+		displayOnMap.done( function(data) {
+			console.log(data);
+			var html = "";
+			$.each(data.photos, function(index, value) {
+				//console.log(index);
+				//console.log(value); 
+				html += '<img src="' + value.image_url + '"/>';
+				$('.pictures').html(html);
+				//Grabbing lat and long of pictures
+				Lat = value.latitude;
+				Lng = value.longitude;
+
+				var eachPic = value.image_url;
+				console.log(eachPic);
+				var latlng = new google.maps.LatLng(Lat, Lng);
+				var marker = new google.maps.Marker({
+					position: latlng,
+					map: map,
+					icon: eachPic
+				});
+			});	
+		});
 	});
 };
 
-var showPictures = function(data) {
-	var html = "";
-	$.each(data, function(index, value) {
-		console.log(index);
-		console.log(value); 
-		html += '<img src="' + value.photos.images.url + '"/>';
-		console.log('The latitude is ' + value.photos.latitude);
-		console.log('The longitude is ' + value.photos.longitude);
-	});
-	$('.pictures').html(html);
-};
-
-var getPhotos = function(location) {
-	console.log(location);
-	//parameter to get Photos from 500px API
-	var params = {
-		tag: location,
-		part: 'photos'
-	};
-
-	$.ajax({
-		url: "https://api.500px.com/v1/photos/search?consumer_key=jbENq5GFQfuSSut8cQQSbRH6sRjNoxVehMwJZIwp",
-		data: params,
-		dataType: "json",
-		type: "GET",
-	})
-
-	.done( function(data) {
-		console.log(data);
-		//show the pics from 500px 
-		showPictures(data);
-
-	});
-};
-
-$(document).ready(function() {
-	$('.location-getter').submit( function(e) {
-		e.preventDefault();
-		$('.pictures').html('');
-		var location = $(this).find("input[name='location']").val();
+function geocodeAddress(geocoder, resultsMap) {
+	var address = document.getElementById('address').value;
+	geocoder.geocode({'address': address}, function(results, status) {
+		if (status === 'OK') {
+			console.log(results[0].geometry.location);
+			resultsMap.setCenter(results[0].geometry.location);
+			var latitude = results[0].geometry.location.lat(); 
+			var longitude = results[0].geometry.location.lng();
 		getPhotos(location);
 	}); 
 });
